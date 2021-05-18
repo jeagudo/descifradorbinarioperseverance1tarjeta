98966af968a0c0c90805ffb9add67ea5494d31f8
J. Enrique Agudo <jeagudo@unex.es>
2021-05-18 10:27:20 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -51,11 +51,5 @@ Numero = 0
 convertirALetra = false
 LETRAS = ["-", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 basic.forever(function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+    basic.clearScreen()
 })
